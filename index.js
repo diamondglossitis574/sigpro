2e72286a8954490878766bc10dd179f705f1b60b
Natxo <1172351+natxocc@users.noreply.github.com>
2026-03-13 15:32:12 +0100

    Refactor exports in index.js for better structure

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// This is the main entry point of the package
-// Directly exports your plugin from sigpro.js
-
-// If your sigpro.js already exports the plugin as default, you can do:
-export { default } from './sigpro.js';
-
-// Or if you prefer CommonJS:
-// module.exports = require('./sigpro.js');
+export * from './sigpro.js';
+import * as sigpro from './sigpro.js';
+export default sigpro;
