650d6e7e605ae52145955c94328bd6d236422602
Natxo <1172351+natxocc@users.noreply.github.com>
2026-03-13 15:13:27 +0100

    Create index.js as entry point for the package
    Add main entry point for the package with export options.

---
+++ index.js
@@ -0,0 +1,8 @@
+// This is the main entry point of the package
+// Directly exports your plugin from sigpro.js
+
+// If your sigpro.js already exports the plugin as default, you can do:
+export { default } from './sigpro.js';
+
+// Or if you prefer CommonJS:
+// module.exports = require('./sigpro.js');
