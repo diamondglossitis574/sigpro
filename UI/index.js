d24bad018eb4aeca0661cdb856705ded797cbc6a
natxocc <natxocc@gmail.com>
2026-03-18 23:36:01 +0100

    prepare to use sigproui

--- UI/index.js
+++ UI/index.js
@@ -1,12 +1,6 @@
-/**
- * SigProUI - Biblioteca de componentes UI basada en SigPro, Tailwind CSS y DaisyUI
- * @version 1.0.0
- */
-
-// Importar estilos de la librería
+// index.js
 import "./sigproui.css";
 
-// Importar todos los componentes para registrarlos automáticamente
 import "./components/Button.js";
 import "./components/Card.js";
 import "./components/Checkbox.js";
@@ -25,13 +19,51 @@ import "./components/Rating.js";
 import "./components/Tab.js";
 import "./components/Toast.js";
 
+export { default as Button } from "./components/Button.js";
+export { default as Card } from "./components/Card.js";
+export { default as Checkbox } from "./components/Checkbox.js";
+export { default as ColorPicker } from "./components/ColorPicker.js";
+export { default as DatePicker } from "./components/DatePicker.js";
+export { default as Dialog } from "./components/Dialog.js";
+export { default as Drawer } from "./components/Drawer.js";
+export { default as Dropdown } from "./components/Dropdown.js";
+export { default as Fab } from "./components/Fab.js";
+export { default as Input } from "./components/Input.js";
+export { default as Loading } from "./components/Loading.js";
+export { default as Menu } from "./components/Menu.js";
+export { default as Radio } from "./components/Radio.js";
+export { default as Range } from "./components/Range.js";
+export { default as Rating } from "./components/Rating.js";
+export { default as Tab } from "./components/Tab.js";
+export { default as Toast } from "./components/Toast.js";
+
+export const components = [
+  "Button",
+  "Card",
+  "Checkbox",
+  "ColorPicker",
+  "DatePicker",
+  "Dialog",
+  "Drawer",
+  "Dropdown",
+  "Fab",
+  "Input",
+  "Loading",
+  "Menu",
+  "Radio",
+  "Range",
+  "Rating",
+  "Tab",
+  "Toast",
+];
+
 // Exportar versión
 export const version = "1.0.0";
 export const name = "SigProUI";
 
-// Exportar información de la librería
 export default {
   version,
   name,
   description: "Biblioteca de componentes UI basada en SigPro, Tailwind CSS y DaisyUI",
+  components,
 };
--- index.js
+++ index.js
@@ -8,7 +8,11 @@ export * from './sigpro.js';
 import sigproRouter from './sigpro-router-plugin.js';
 export { sigproRouter };
 
-// 3. Default export for the global namespace (optional)
-// Combines core logic and the router plugin into a single object
+// 3. Re-export UI components
+// Users can import components like: import { Button, Input, Card } from 'sigpro';
+export * from './UI/index.js';
+
+// 4. Default export for the global namespace (optional)
+// Combines core logic, router plugin, and UI components into a single object
 import * as sigpro from './sigpro.js';
 export default { ...sigpro, sigproRouter };
