1911ceff84a93a8fb3d4ae86a4600158a9647287
natxocc <natxocc@gmail.com>
2026-03-20 01:40:13 +0100

    remove sigproui

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 // index.js
 export * from './packages/sigpro/sigpro.min.js';
 export { default as sigproRouter } from './packages/sigpro/plugin.min.js';
-export * from './packages/sigproui/index.js';
