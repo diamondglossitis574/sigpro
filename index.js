1ba4250bcc44be8cb10739a5e295a6e98f9f164f
Natxo <1172351+natxocc@users.noreply.github.com>
2026-03-21 09:59:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // index.js
-export * from './packages/sigpro/sigpro.min.js';
-export { default as sigproRouter } from './packages/sigpro/plugin.min.js';
+export * from './packages/sigpro/sigpro.js';
+export { default as sigproRouter } from './packages/sigpro/plugin.js';
